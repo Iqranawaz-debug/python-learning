7c533219de095913dcd1558ca586198ea8d9214f
Iqranawaz-debug <iqranawaz9353@gmail.com>
2026-09-21 19:25:46 +0500

    Day 5 completed

--- day5/practice.py
+++ day5/practice.py
@@ -32,14 +32,3 @@ student = {
 for key,value in student.items():
     print(key,":","value")
 
-#count with dictionary
-a = ['apple', 'banana', 'apple', 'orange', 'banana', 'banana']
-
-freq = {}
-for item in a:
-    if item in freq:
-        freq[item] += 1
-    else:
-        freq[item] = 1
-
-print(freq)
